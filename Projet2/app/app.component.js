91c30cfcf1c34ef53affc70af27f5a48451abb5a
Matthiasreimund <matthias.reimund@ynov.com>
2016-04-11 11:29:28 +0200

    favoris
    la page ou sera mis les favoris utilisateurs

--- Projet2/app/app.component.js
+++ Projet2/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './carousel.component'], function(exports_1, context_1) {
+System.register(['angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,32 +10,49 @@ System.register(['angular2/core', './carousel.component'], function(exports_1, c
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, carousel_component_1;
-    var AppComponent;
+    var core_1;
+    var Favori, AppComponent, FAVORIES;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (carousel_component_1_1) {
-                carousel_component_1 = carousel_component_1_1;
             }],
         execute: function() {
+            Favori = (function () {
+                function Favori() {
+                }
+                return Favori;
+            }());
+            exports_1("Favori", Favori);
             AppComponent = (function () {
                 function AppComponent() {
+                    this.title = 'your Favories';
+                    this.favories = FAVORIES;
                 }
+                AppComponent.prototype.onSelect = function (favori) { this.selectedFavori = favori; };
                 AppComponent = __decorate([
                     core_1.Component({
-                        selector: 'my-app',
-                        template: "\n    <h1>{{title}}</h1>\n    <h1><div class=\"title\">CINELOVERS</div></h1>\n    <h2>\n    <div class=\"wrapper\">\n          <css-carousel></css-carousel>\n          </div>\n\n    ",
-                        styles: ["\n\n   .title{\n   text-align: center;\n   }\n    .wrapper{\n      width: 60%;\n      margin: 60px auto;\n  "],
-                        directives: [carousel_component_1.CSSCarouselComponent]
+                        selector: 'favori',
+                        template: "\n    <h1>{{title}}</h1>\n    <h2>Here your favori</h2>\n    <ul class=\"favories\">\n      <li *ngFor=\"#favori of favories\"\n        [class.selected]=\"favori === selectedFavori\"\n        (click)=\"onSelect(favori)\">\n        <span class=\"badge\">{{favori.id}}</span> {{favori.name}}\n      </li>\n    </ul>\n    <div *ngIf=\"selectedFavori\">\n      <h2>{{selectedFavori.name}} details!</h2>\n      <div><label>id: </label>{{selectedFavori.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"selectedFavori.name\" placeholder=\"name\"/>\n      </div>\n    </div>\n  ",
+                        styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .favories {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .favories li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .favories li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .favories li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .favories .text {\n      position: relative;\n      top: -3px;\n    }\n    .favories .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             }());
             exports_1("AppComponent", AppComponent);
+            FAVORIES = [
+                { "id": 11, "name": "Mr. Nice" },
+                { "id": 12, "name": "Narco" },
+                { "id": 13, "name": "Bombasto" },
+                { "id": 14, "name": "Celeritas" },
+                { "id": 15, "name": "Magneta" },
+                { "id": 16, "name": "RubberMan" },
+                { "id": 17, "name": "Dynama" },
+                { "id": 18, "name": "Dr IQ" },
+                { "id": 19, "name": "Magma" },
+                { "id": 20, "name": "Tornado" }
+            ];
         }
     }
 });
--- Projet2/app/app.component.ts
+++ Projet2/app/app.component.ts
@@ -1,27 +1,94 @@
 import {Component} from 'angular2/core';
-import {CSSCarouselComponent} from './carousel.component';
-
+export class Favori {
+  id: number;
+  name: string;
+}
 @Component({
-  selector: 'my-app',
+  selector: 'favori',
   template:`
     <h1>{{title}}</h1>
-    <h1><div class="title">CINELOVERS</div></h1>
-    <h2>
-    <div class="wrapper">
-          <css-carousel></css-carousel>
-          </div>
-
-    `,
+    <h2>Here your favori</h2>
+    <ul class="favories">
+      <li *ngFor="#favori of favories"
+        [class.selected]="favori === selectedFavori"
+        (click)="onSelect(favori)">
+        <span class="badge">{{favori.id}}</span> {{favori.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedFavori">
+      <h2>{{selectedFavori.name}} details!</h2>
+      <div><label>id: </label>{{selectedFavori.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedFavori.name" placeholder="name"/>
+      </div>
+    </div>
+  `,
   styles:[`
-
-   .title{
-   text-align: center;
-   }
-    .wrapper{
-      width: 60%;
-      margin: 60px auto;
-  `],
-  directives: [CSSCarouselComponent]
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .favories {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .favories li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .favories li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .favories li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .favories .text {
+      position: relative;
+      top: -3px;
+    }
+    .favories .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
 export class AppComponent {
+  title = 'your Favories';
+  favories = FAVORIES;
+  selectedFavori: Favori;
+  onSelect(favori: Favori) { this.selectedFavori = favori; }
 }
+var FAVORIES: Favori[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
